d3c3d09387293a3a7b43fc8957d55f443152ba07
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 11:32:12 -0700

    Created leapYears Function
    I added arguements to check if a year is a leap year or not.
    
    If it is a leap year it returns true and flase for all aelse.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // First check if year is divisible by 4
+  if (year % 4 === 0) {
+    // If year is divisible by 100
+    if (year % 100 === 0) {
+      // It must also be divisible by 400 to be a leap year
+      return year % 400 === 0;
+    }
+    // If not divisible by 100, it is a leap year
+    return true;
+  }
+  // If not divisible by 4, it's not a leap year
+  return false;
 };
 
 // Do not edit below this line
