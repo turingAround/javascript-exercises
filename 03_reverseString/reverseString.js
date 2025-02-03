0124f40755daa274c2d464ce4280c8ae00044b53
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 11:11:13 -0700

    Added reverse loop to reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+// Function to reverse order of the string that's passed.
+const reverseString = function(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
