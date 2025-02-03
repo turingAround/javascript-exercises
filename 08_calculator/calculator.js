5b6a152294277f96f9140ec3c052a94b95d4327d
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 11:43:42 -0700

    Created functions for each type of calculation

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
+
 
 // Do not edit below this line
 module.exports = {
