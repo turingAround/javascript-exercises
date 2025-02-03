ac3bc673a26a5f6b389f839c2bc98f83007668fc
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 13:13:31 -0700

    created function
    added arguements to compare people by age.
    algorith takes birth year and subtracts from death year.
    in the case of someone still living it subtracts from the current year.
    it then compares each person based on the age and returns the oldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+    const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+    
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
