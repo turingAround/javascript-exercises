d3931c90cf228a192f8134939247c77e8a64526f
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 10:42:37 -0700

    Added loop to function.
    I added a loop to the repeatString function so that it will
    loop through the function as many times as told.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
