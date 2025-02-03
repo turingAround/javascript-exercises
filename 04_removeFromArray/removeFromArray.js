cacbe40ad92623da7489008a942cdeba6774f24d
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 11:16:58 -0700

    Created Function
    I added in a function to first create an array and from there
    remove all the neccesary bits from the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(...args) {
+    const array = args[0];  // First argument is the array
+    const elementsToRemove = args.slice(1);  // Rest of the arguments are elements to remove
+    
+    return array.filter(item => !elementsToRemove.includes(item));
+  };
+  
 // Do not edit below this line
 module.exports = removeFromArray;
