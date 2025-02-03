de6291beaae28d38705b82adfa44b7d2ca5fae0b
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-03 12:37:54 -0700

    Added arguements to function.
    Added arguements to take a string input and make sure that it's
    case insensitive and removes all none alphabetical characters,
    i.e. punction.
    
    added arguements to compare the beginning and end characters and loop
    towards the centre, stopping as soon as the loop encounters
    any characters that don't match.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    // Convert string to lowercase and remove non-alphanumeric characters
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Loop through the string, comparing characters from start and end
+    for (let i = 0; i < cleanStr.length / 2; i++) {
+        // Compare characters from start and end moving inward
+        if (cleanStr[i] !== cleanStr[cleanStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
